81c64799a2a1851c89310bb929696ed800bddba3
Balkrishna Pandey <sachit.nep@gmail.com>
2015-03-20 21:28:13 +0545

    Factor not equal

--- Questions.java
+++ Questions.java
@@ -259,6 +259,11 @@ public class Questions {
 			System.out.println("<<<<<<>>>>>>>>>>>>>>>><<<<<<<<<<<<<>>>>>>>><<<<<>>>>");
 			factorEqual(10,33);
 			factorEqual(9,10);
+
+			/* Function isMeera */
+			System.out.println("<<<<<<>>>>>>>>>>>>>>>><<<<<<<<<<<<<>>>>>>>><<<<<>>>>");
+			isMeera(new int[]{-4, 0, 1, 0, 2, 1});
+			isMeera(new int[]{-8, 0, 0, 8, 0});
 		}
 	
 
@@ -1033,6 +1038,48 @@ public class Questions {
 			return 1;
 		}
 		System.out.println("Return 0 factor not equal");
+		return 0;
+	}
+
+	/*
+	2. Define a Meera array to be an array a if it satisfies two conditions:
+	(a) a[i] is less than i for i = 0 to a.length-1. 
+	(b) sum of all elements of a is 0.
+
+	For example, {-4, 0, 1, 0, 2, 1} is a Meera array because
+	-4 = a[0] < 0
+	0 = a[1] < 1
+	1 = a[2] < 2
+	0 = a[3] < 3
+	2 = a[4] < 4
+	1 = a[5] < 5
+
+	and -4 + 0 + 1 + 0 + 2 + 1 = 0
+
+	{-8, 0, 0, 8, 0} is not a Meera array because a[3] is 8 which is not less than 3. Thus condition (a) fails. {-8, 0, 0, 2, 0} is not a Meera array because -8 + 2 = -6 not equal to zero. Thus condition (b) fails.
+
+	Write a function named isMeera that returns 1 if its array argument is a Meera array. Otherwise it returns 0.
+
+	If you are programming in Java or C#, the function signature is
+	   int isMeera (int[ ] a)
+
+	If you are programming in C or C++, the function signature is
+	   int isMeera (int a[ ], int len) where len is the number of elements in the array.
+	*/
+
+	static int isMeera(int[] a){
+		int sum=0;
+		for(int i=0; i<a.length; i++){
+			if(a[i]>=i){
+				System.out.println("a[i] is greater than i for i = 0 to a.length-1");
+				return 0;
+			}
+			sum+=a[i];
+		}
+		if(sum==0){
+			System.out.println("return 1");
+			return 1;
+		}
 		return 0;
 	}
 }
